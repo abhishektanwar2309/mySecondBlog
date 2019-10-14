0a23084510f735ede6b3d909e8f4df407370a820
abhishektanwar2309 <abhishektanwar2309@gmail.com>
2019-10-14 18:32:24 +0530

    My django girls second app , first commit

---
+++ youcan/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abhishek2309/Desktop/FCS/projectMe/djangoGirlsNew/youcan/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
